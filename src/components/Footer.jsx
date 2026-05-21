e9caa12d8da87b3b8f34180339ea64ee2b6f7528
Jason Dagana <j.dagana@teflonhub.com>
2026-05-21 01:10:04 +0100

    fixed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+        <div className="footer-i">
+          <h3>About</h3>
+          <p>
+            first name of group members
+          </p>
+        </div>
+ 
+        <hr />
+ 
+        <div className="footer-ii">
+          <span>©2026 Design by Amaka & Ifeoma A. </span>
+          <div className="flex">
+             <span>Built by GroupName. All rights reserved</span>
+            <span>Terms &amp; Condition</span>
+          </div>
+        </div>
+      </footer>
   )
 }
 
