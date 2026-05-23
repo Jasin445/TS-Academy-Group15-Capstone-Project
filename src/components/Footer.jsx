4d01eadd37c5a565ae61776140faac3e345fed6a
Jason Dagana <j.dagana@teflonhub.com>
2026-05-23 13:09:00 +0100

    fixed scroll issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
           <span>©2026 Design by Amaka & Ifeoma A. </span>
           <div className="flex">
              <span>Built by Group 15. <br className="break-footer-note" />All rights reserved</span>
-            <span>Terms &amp; Condition</span>
+            <span>TSAcademy</span>
           </div>
         </div>
       </footer>
