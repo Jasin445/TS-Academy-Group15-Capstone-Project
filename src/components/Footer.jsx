6ffcbefa7c90b6cdf974e2f9c2352a21cf46faad
Jason Dagana <j.dagana@teflonhub.com>
2026-05-24 16:56:47 +0100

    made changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,38 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-        <div className="footer-i">
-          <h3>About</h3>
-          <p>
-            first name of group members
-          </p>
-        </div>
- 
-        <hr />
- 
-        <div className="footer-ii">
-          <span>©2026 Design by Amaka & Ifeoma A. </span>
-          <div className="flex">
-             <span>Built by Group 15. <br className="break-footer-note" />All rights reserved</span>
-            <span>TSAcademy</span>
-          </div>
-        </div>
-      </footer>
-  )
-}
+      <div className="footer-i">
+        <h3>About</h3>
+        <p>first name of group members</p>
+      </div>
 
-export default Footer
+      <hr />
+
+      <div className="footer-ii">
+        <span>©2026 Design by Amaka & Ifeoma A. </span>
+        <div className="flex">
+          <span>
+            Built by{" "}
+            <Link
+              to={
+                "https://github.com/Jasin445/TS-Academy-Group15-Capstone-Project"
+              }
+              className="footer-link"
+            >
+              Group 15.
+            </Link>{" "}
+            <br className="break-footer-note" />
+            All rights reserved
+          </span>
+          <Link to={"https://tsacademyonline.com/"} className="footer-link">
+            TSAcademy
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
